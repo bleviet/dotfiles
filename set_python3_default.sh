1dfb2a6245114e5147330f6c75cfab593d9a6e1b
Bach Le Viet <bleviet@gmail.com>
2020-09-03 23:42:11 +0200

    added script to set python3 as default

---
+++ set_python3_default.sh
@@ -0,0 +1,10 @@
+# You already have Python3 but 
+# don't care about the version 
+sudo ln -s /usr/bin/python3 /usr/bin/python -f
+
+# Same for pip
+sudo apt install -y python3-pip
+sudo ln -s /usr/bin/pip3 /usr/bin/pip
+
+# Confirm the new version of Python: 3
+python --version
