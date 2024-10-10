7811e2afa25497c501124eb50a122ccb77bbbd81
Le Viet Bach <bleviet@gmail.com>
2024-10-10 23:15:16 +0200

    replace telescope with fzf for picker
    remove global.lua, use options.lua instead

--- stow/nvim/.config/nvim/lua/config/global.lua
+++
@@ -1,2 +0,0 @@
--- Always return the current working directory for searches
-vim.g.root_spec = { "cwd" }
--- stow/nvim/.config/nvim/lua/config/options.lua
+++ stow/nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Always return the current working directory for searches
+vim.g.root_spec = { "cwd" }
+
+-- replace telescope with fzf for picker
+vim.g.lazyvim_picker = "fzf"
