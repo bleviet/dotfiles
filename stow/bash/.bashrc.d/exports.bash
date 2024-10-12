d75bc6199666f6227a1b716f9cbd9f122d1d0d53
Le Viet Bach <bleviet@gmail.com>
2024-10-12 21:49:14 +0200

    Set nvim as default editor

--- stow/bash/.bashrc.d/exports.bash
+++ stow/bash/.bashrc.d/exports.bash
@@ -5,5 +5,7 @@ export GIT_PS1_SHOWUNTRACKEDFILES=1
 # Set LS_COLORS based on the Dracula theme
 export LS_COLORS='di=35:ln=36:ex=32:bd=33;44:cd=33;41:pi=35:so=33:*.tar=31:*.gz=31:*.zip=31:*.jpg=35:*.jpeg=35:*.png=35:*.gif=35:*.bmp=35:*.txt=37'
 
+export EDITOR=nvim
+
 # PATH
 export PATH=$HOME/miniconda3/bin:$HOME/.fzf/bin:$HOME/.local/bin:$PATH
