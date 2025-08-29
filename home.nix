e423ef838e288e9de5d4195df02babdfcec78135
Bach Le Viet <bleviet@gmail.com>
2025-08-29 23:56:03 +0200

    Add example nix configuration file

---
+++ home.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+{
+  home.stateVersion = "24.05";
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    # Neueste Versionen, direkt von Nix
+    vscode
+    neovim
+    git
+    gh
+    ripgrep
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "Bach Le Viet";
+    userEmail = "bleviet@email.com";
+  };
+
+  # Weitere Konfigurationen...
+}
