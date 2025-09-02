a8a66666926c88adb4719cd498af6241df077ed9
Bach Le Viet <bleviet@gmail.com>
2025-09-03 00:15:27 +0200

    Add functions to quickly gitk to a file or change directory to the selected file with fzf

--- stow/bash/.bashrc.d/functions.bash
+++ stow/bash/.bashrc.d/functions.bash
@@ -2,3 +2,24 @@
 parse_git_branch() {
   git branch 2>/dev/null | grep '\*' | sed 's/* //'
 }
+
+# View gitk for a file selected with fzf
+gkf() {
+  local file
+  # Run fzf and store the selected file path in the 'file' variable
+  file=$(fzf)
+
+  # Only run gitk if a file was actually selected (i.e., you didn't press Esc)
+  if [[ -n "$file" ]]; then
+      gitk "$file"
+  fi
+}
+
+# cdf: Change directory to the directory containing the file selected by fzf.
+cdf() {
+  local file
+  file=$(fzf)
+  if [[ -n "$file" ]]; then
+    cd "$(dirname "$file")"
+  fi
+}
